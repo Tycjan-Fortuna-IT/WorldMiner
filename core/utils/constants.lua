7a6c1a19d5d25aede8f70fc92727b4f9cde25a04
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-07-23 11:17:18 +0200

    Many pickaxe tiers as constants

---
+++ core/utils/constants.lua
@@ -0,0 +1,40 @@
+local C = {}
+
+C.pickaxe_tiers = {
+    'Wood', 'Plastic', 'Bone', 'Alabaster', 'Lead', 'Zinc', 'Tin', 'Salt', 'Bauxite', 'Borax',
+    'Bismuth', 'Amber', 'Galena', 'Calcite', 'Aluminium', 'Silver', 'Gold', 'Copper', 'Marble',
+    'Brass', 'Flourite', 'Platinum', 'Nickel', 'Iron', 'Manganese', 'Apatite', 'Uraninite',
+    'Turquoise', 'Hematite', 'Glass', 'Magnetite', 'Concrete', 'Pyrite', 'Steel', 'Zircon',
+    'Titanium', 'Silicon', 'Quartz', 'Garnet', 'Flint', 'Tourmaline', 'Beryl', 'Topaz',
+    'Chrysoberyl', 'Chromium', 'Tungsten', 'Corundum', 'Tungsten', 'Diamond', 'Netherite',
+    'Penumbrite', 'Meteorite', 'Crimtane', 'Obsidian', 'Demonite', 'Mythril', 'Adamantite',
+    'Chlorophyte', 'Densinium', 'Luminite', 'Adamantium', 'Ebonite', 'Fools_Gold', 'Gorgonite',
+    'Hellstone', 'Iridium', 'Jade', 'Kyanite', 'Lapis_Lazuli', 'Moonstone', 'Nether_Gold',
+    'Orichalcum', 'Prismarite', 'Quantum_Crystal', 'Rainbow_Gem', 'Sapphire', 'Thaumium',
+    'Vibranium', 'Warp_Stone', 'Xenotime', 'Yttrium', 'Zorgium',
+    'Celestial', 'Cosmic', 'Ethereal', 'Galactic', 'Infinity', 'Mythical', 'Omega', 'Transcendent',
+    'Supreme', 'Primordial', 'Apex', 'Ultimate', 'Exalted', 'Ascendant', 'Immortal', 'Titanic',
+    'Almighty', 'Epic', 'Legendary', 'Mythic', 'Ancient', 'Divine', 'Fabled', 'Eternal', 'Godly',
+    'Omnipotent', 'Transcendental', 'Unreal', 'Mythological', 'Supernatural', 'Infinite', 'Divine',
+    'Mythical', 'Unparalleled', 'Mythopoeic', 'Peerless', 'Unearthly', 'Beyond',
+    'Supersonic', 'Galactic', 'Celestial', 'Cosmic', 'Astral', 'Epic', 'Mythic', 'Mystic', 'Transcendent',
+    'Infernal', 'Eternal', 'Mythological', 'Celestial', 'Omnipotent', 'Alpha', 'Omega', 'Divine', 'Prismatic',
+    'Apex', 'Supreme', 'Enchanted', 'Imbued', 'Resplendent', 'Sublime', 'Mythopoeic', 'Empyrean', 'Chimeric',
+    'Pristine', 'Peerless', 'Seraphic', 'Omniscient', 'Ethereal', 'Sublime', 'Primordial', 'Infinite', 'Arcane',
+    'Mythical', 'Enigmatic', 'Empyreal', 'Exalted', 'Immutable', 'Sovereign', 'Elysian', 'Unfathomable', 'Legendary',
+    'Transcendental', 'Unreal', 'Imperial', 'Ultimate', 'Unbound', 'Magnificent', 'Regal', 'Venerable', 'Mythopoeic',
+    'Unsurpassable', 'Immeasurable', 'Stellar', 'Unearthly', 'Empyrean', 'Radiant', 'Apocalyptic', 'Sapient', 'Divine',
+    'Exalted', 'Peerless', 'Ascendant', 'Godly', 'Supernatural', 'Transcendent', 'Infinite', 'Omnipotent', 'Eternal',
+    'Celestial', 'Mythic', 'Unparalleled', 'Mythological', 'Mythical', 'Mythopoeic', 'Legendary', 'Immortal', 'Ultimate',
+    'Primordial', 'Supreme', 'Cosmic', 'Divine', 'Ethereal', 'Astral', 'Galactic', 'Transcendent', 'Omega', 'Supersonic',
+    'Enchanted', 'Mythical', 'Imbued', 'Infernal', 'Resplendent', 'Prismatic', 'Celestial', 'Empyreal', 'Sovereign',
+    'Chimeric', 'Pristine', 'Peerless', 'Seraphic', 'Sublime', 'Omniscient', 'Ethereal', 'Arcane', 'Divine',
+    'Mythological',
+    'Mythical', 'Enigmatic', 'Exalted', 'Immutable', 'Regal', 'Elysian', 'Unfathomable', 'Legendary', 'Unreal',
+    'Imperial',
+    'Unbound', 'Ultimate', 'Magnificent', 'Venerable', 'Unsurpassable', 'Immeasurable', 'Unearthly', 'Radiant',
+    'Empyrean'
+}
+
+
+return C
