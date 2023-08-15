6dfeb7ed8d0d1e8b08982a0d7f8c8a2f8dd273a0
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-08-15 22:11:59 +0200

    Usage of FLH as a prototype helper

---
+++ prototypes/items.lua
@@ -0,0 +1,160 @@
+-- _____________________________________________________________________________________________
+-- This file was automatically generated Tue Aug 15 2023 17:42:34 UTC by FactorioLuaHelper.
+-- _____________________________________________________________________________________________
+
+data:extend({
+{
+	name = "blue-coin",
+	type = "item",
+	icons = {
+		{
+			icon = "__base__/graphics/icons/coin.png",
+			icon_size = 64,
+			tint = { r = 0.18, g = 0.51, b = 1, a = 0.86 },
+		},
+	},
+	order = "a[coin]",
+	stack_size = 100000,
+},
+{
+	name = "blue-crystal",
+	type = "item",
+	icons = {
+		{
+			icon = "__WorldMiner__/graphics/blue-crystal/blue-crystal-icon.png",
+			icon_size = 64,
+		},
+	},
+	order = "c",
+	stack_size = 1000,
+},
+{
+	name = "crystal-extractor",
+	type = "item",
+	icons = {
+		{
+			icon = "__WorldMiner__/graphics/crystal-extractor/crystal-extractor-icon.png",
+			icon_size = 64,
+		},
+	},
+	order = "c",
+	stack_size = 10,
+},
+{
+	name = "green-banner",
+	type = "item",
+	icons = {
+		{
+			icon = "__WorldMiner__/graphics/banner/green-banner-icon.png",
+			icon_size = 64,
+		},
+	},
+	order = "b[banner]-green",
+	stack_size = 1,
+	place_result = "green-banner",
+},
+{
+	name = "purple-banner",
+	type = "item",
+	icons = {
+		{
+			icon = "__WorldMiner__/graphics/banner/purple-banner-icon.png",
+			icon_size = 64,
+		},
+	},
+	order = "b[banner]-purple",
+	stack_size = 1,
+	place_result = "purple-banner",
+},
+{
+	name = "red-banner",
+	type = "item",
+	icons = {
+		{
+			icon = "__WorldMiner__/graphics/banner/red-banner-icon.png",
+			icon_size = 64,
+		},
+	},
+	order = "b[banner]-red",
+	stack_size = 1,
+	place_result = "red-banner",
+},
+{
+	name = "yellow-banner",
+	type = "item",
+	icons = {
+		{
+			icon = "__WorldMiner__/graphics/banner/yellow-banner-icon.png",
+			icon_size = 64,
+		},
+	},
+	order = "b[banner]-yellow",
+	stack_size = 1,
+	place_result = "yellow-banner",
+},
+{
+	name = "gray-banner",
+	type = "item",
+	icons = {
+		{
+			icon = "__WorldMiner__/graphics/banner/gray-banner-icon.png",
+			icon_size = 64,
+		},
+	},
+	order = "b[banner]-gray",
+	stack_size = 1,
+	place_result = "gray-banner",
+},
+{
+	name = "black-banner",
+	type = "item",
+	icons = {
+		{
+			icon = "__WorldMiner__/graphics/banner/black-banner-icon.png",
+			icon_size = 64,
+		},
+	},
+	order = "b[banner]-black",
+	stack_size = 1,
+	place_result = "black-banner",
+},
+{
+	name = "orange-banner",
+	type = "item",
+	icons = {
+		{
+			icon = "__WorldMiner__/graphics/banner/orange-banner-icon.png",
+			icon_size = 64,
+		},
+	},
+	order = "b[banner]-orange",
+	stack_size = 1,
+	place_result = "orange-banner",
+},
+{
+	name = "white-banner",
+	type = "item",
+	icons = {
+		{
+			icon = "__WorldMiner__/graphics/banner/white-banner-icon.png",
+			icon_size = 64,
+		},
+	},
+	order = "b[banner]-white",
+	stack_size = 1,
+	place_result = "white-banner",
+},
+{
+	name = "blue-banner",
+	type = "item",
+	icons = {
+		{
+			icon = "__WorldMiner__/graphics/banner/blue-banner-icon.png",
+			icon_size = 64,
+		},
+	},
+	order = "b[banner]-blue",
+	stack_size = 1,
+	place_result = "blue-banner",
+},
+})
